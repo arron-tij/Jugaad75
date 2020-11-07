34128f6456cd30ebe4c22fb322ec65fa3f534261
Ronit Raj <ronitraj.tg@gmail.com>
2020-11-07 14:34:34 +0530

    Added attendance tracking
    Added feature to get current coordinatess and mark attendance automatically. Also included requirements.txt file

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
-from .models import classroom,Lecture,lecture_copy
+from .models import classroom,Lecture,lecture_copy,sdata
 # Register your models here.
 
 admin.site.register(classroom)
 admin.site.register(Lecture)
-
+admin.site.register(sdata)
 admin.site.register(lecture_copy)
